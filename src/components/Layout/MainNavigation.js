cc1d215a521651263cbf7237ce7ef5433e43eaaf
Alex Bortnik <bortnik.ar@gmail.com>
2022-04-24 13:56:58 +0400

    Use context in navigation

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,8 +1,18 @@
-import { Link } from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
+import {useContext} from 'react';
+import {AuthContext} from '../../context/auth-context';
 
 const MainNavigation = () => {
+  const authContext = useContext(AuthContext);
+  const history = useHistory();
+
+  const logOutHandler = () => {
+    history.push('/auth');
+    authContext.logOut();
+  };
+
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -10,15 +20,22 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!authContext.isLoggedIn &&
+            <li>
+              <Link to='/auth'>Login</Link>
+            </li>
+          }
+
+          {authContext.isLoggedIn &&
+            <>
+              <li>
+                <Link to='/profile'>Profile</Link>
+              </li>
+              <li>
+                <button onClick={logOutHandler}>Logout</button>
+              </li>
+            </>
+          }
         </ul>
       </nav>
     </header>
